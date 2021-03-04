e530dc57a8c7359c49fd9cd7dad6cbc591990718
Saro Deravanesian <saro.deravanesian@gmail.com>
2021-03-04 14:30:43 +0400

    hashing password

--- express_example/users/user.entity.js
+++ express_example/users/user.entity.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -24,5 +25,14 @@ const schema = new Schema({
         required: true,
     },
 }, {collection : 'users'});
+
+schema.pre('save', function(next) {
+    if(this.isModified('password')) {
+        const salt = bcrypt.genSaltSync();
+        this.password = bcrypt.hashSync(this.password, salt);
+    }
+
+    next();
+})
 
 module.exports = mongoose.model('User', schema);
