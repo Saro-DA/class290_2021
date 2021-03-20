c635cc9549633a36fa0beeaefbe763298d8f8e3a
Rebecca <asryan.rebecca@gmail.com>
2021-03-20 21:57:40 +0400

    Check locking user with unsuccessful login

--- express_example/test/auth/auth.e2e.spec.js
+++ express_example/test/auth/auth.e2e.spec.js
@@ -7,13 +7,28 @@ const expect = chai.expect;
 describe("Testing auth API", function () {
 
     before(async function () {
+        await request(app)
+            .post('/users')
+            .send({
+                username: "willsmith",
+                password: "12345",
+                firstName: "Will",
+                lastName: "Smith",
+                role: "customer",
+            })
+            .expect(201)
+            .expect(res => {
+                return expect(res.body).to.have.property('_id');
+            });
+
         return request(app)
             .post('/users')
             .send({
                 username: "johnconor",
                 password: "1234",
                 firstName: "John",
-                lastName: "Conor"
+                lastName: "Conor",
+                role: "admin"
             })
             .expect(201)
             .expect(res => {
@@ -26,7 +41,6 @@ describe("Testing auth API", function () {
     })
 
     describe("Testing login API /auth/login [POST]", function () {
-
         it("Logs in a user successfully", function () {
             return request(app)
                 .post('/auth/login')
@@ -39,5 +53,28 @@ describe("Testing auth API", function () {
                     return expect(res.body).to.have.property('token');
                 })
         })
+    })
+
+    describe('Testing locking user after 3 unsuccessful successive logins', function () {
+        it('It should lock the user after 3 failed logins.', async function () {
+            await request(app).post('/auth/login').send({
+                username: "willsmith",
+                password: "wrong"
+            });
+
+            await request(app).post('/auth/login').send({
+                username: "willsmith",
+                password: "wrong"
+            });
+
+            await request(app).post('/auth/login').send({
+                username: "willsmith",
+                password: "wrong"
+            })
+                .expect(423)
+                .expect((res) => {
+                    return expect(res.body).eql({ message: "The user is locked!" });
+                });;
+        });
     })
 })
