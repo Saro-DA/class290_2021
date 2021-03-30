da518ae67fd6be636be181d1f899873c38fee95f
Saro Deravanesian <saro.deravanesian@gmail.com>
2021-03-30 13:51:30 +0400

    setting up react application

--- express_example/src/app.js
+++ express_example/src/app.js
@@ -11,16 +11,18 @@ const { writeInFile, readFromFile } = require('./commons/util');
 const { handleError } = require('./commons/middlewares/error-handler.middleware');
 const asyncHandler = require('express-async-handler');
 const { jwtMiddleware } = require('./commons/middlewares/auth.middleware');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.use(jwtMiddleware.unless({
-    path: [
-        '/auth/login',
-        { url: '/users', methods: ['POST'] }
-    ]
-}));
+// app.use(jwtMiddleware.unless({
+//     path: [
+//         '/auth/login',
+//         { url: '/users', methods: ['POST'] }
+//     ]
+// }));
 
 app.use('/users', users);
 app.use('/auth', auth);
